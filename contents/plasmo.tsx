8f33a59800683ad43becc122e55188865d54972e
WGB5445 <919603023@qq.com>
2023-11-06 07:18:27 +0800

    fix show content

--- contents/plasmo.tsx
+++ contents/plasmo.tsx
@@ -1,64 +1,81 @@
+import cssText from "data-text:~globals.css";
 import type {
   PlasmoCSConfig,
   PlasmoCSUIJSXContainer,
   PlasmoCSUIProps,
+  PlasmoGetInlineAnchor,
+  PlasmoGetOverlayAnchor,
+  PlasmoGetShadowHostId,
+  PlasmoGetStyle,
   PlasmoRender,
 } from "plasmo";
 import type { FC } from "react";
 import { createRoot } from "react-dom/client";
 import Popup from "~popup";
-
+import styleText from "data-text:../globals.css";
 export const config: PlasmoCSConfig = {
   matches: ["https://twitter.com/*"],
 };
-
-export const getRootContainer = () =>
-  new Promise((resolve) => {
-    const checkInterval = setInterval(() => {
-      const mainDiv = document.querySelector(
-        'div[data-testid="sidebarColumn"]'
-      );
-      if (!mainDiv) return;
-      const rootContainerParent = mainDiv.querySelector(
-        "div > div:nth-child(2) > div > div > div > div > div "
-      );
-      if (rootContainerParent) {
-        clearInterval(checkInterval);
-        const rootContainer = document.createElement("div");
-        rootContainer.classList.add("plasmo-root-container");
-        rootContainer.style.width = "350px";
-        rootContainer.style.height = "370px";
-        rootContainerParent.insertBefore(
-          rootContainer,
-          rootContainerParent.children[2]
-        );
-        resolve(rootContainer);
-      }
-    }, 137);
-  });
-
-const PlasmoOverlay: FC<PlasmoCSUIProps> = () => {
-  return (
-    <span
-      style={{
-        borderRadius: 4,
-        background: "yellow",
-        padding: 4,
-        // position: "absolute",
-        // transform: "translateY(-24px) translateX(42px)",
-      }}
-    >
-      CSUI ROOT CONTAINER
-    </span>
+export const getStyle: PlasmoGetStyle = () => {
+  const style = document.createElement("style");
+  style.textContent = styleText;
+  return style;
+};
+export const getInlineAnchor: PlasmoGetInlineAnchor = async () =>
+  document.querySelector(
+    "div[data-testid='sidebarColumn'] > div > div:nth-child(2) > div > div > div > div:nth-child(2)"
   );
-};
+// export const getRootContainer = async () =>
+//   new Promise((resolve, reject) => {
+//     const checkInterval = setInterval(() => {
+//       const root = document.querySelector("#plasmo-root-container");
+//       if (!root) {
+//         const rootContainerParent = document.querySelector(
+//           "div[data-testid='sidebarColumn'] > div > div:nth-child(2) > div > div > div"
+//         );
+//         if (rootContainerParent) {
+//           const rootContainer = document.createElement("div");
+//           rootContainer.id = "plasmo-root-container";
+//           rootContainerParent.insertBefore(
+//             rootContainer,
+//             rootContainerParent.children[2]
+//           );
+//           console.log("plasmo-root-container", rootContainer);
+//           resolve(rootContainer);
+//         }
+//       }
+//     }, 1000);
+//   });
 
-export const render: PlasmoRender<PlasmoCSUIJSXContainer> = async ({
-  createRootContainer,
-}) => {
-  const rootContainer = await createRootContainer();
-  const root = createRoot(rootContainer);
-  root.render(<Popup />);
-};
+// export const render: PlasmoRender<PlasmoCSUIJSXContainer> = async ({
+//   anchor,
+//   createRootContainer,
+// }) => {
+//   const rootContainer = await createRootContainer();
+//   console.log("rootContainer", rootContainer);
+//   const root = createRoot(rootContainer);
+//   root.render(<Popup />);
+// };
+// export const getRootContainer = () =>
+//   new Promise((resolve) => {
+//     const checkInterval = setInterval(() => {
+//       const root = document.querySelector(".plasmo-root-container");
+//       if (root) resolve(root);
+//       const rootContainerParent = document.querySelector(
+//         'div[data-testid="sidebarColumn"] div > div:nth-child(2) > div > div > div'
+//       );
+//       console.log("rootContainerParent", rootContainerParent);
+//       if (rootContainerParent && rootContainerParent.children.length >= 3) {
+//         clearInterval(checkInterval);
+//         const rootContainer = document.createElement("div");
+//         rootContainer.classList.add("plasmo-root-container");
+//         rootContainerParent.insertBefore(
+//           rootContainer,
+//           rootContainerParent.children[2]
+//         );
+//         resolve(rootContainer);
+//       }
+//     }, 137);
+//   });
 
-export default PlasmoOverlay;
+export default Popup;
